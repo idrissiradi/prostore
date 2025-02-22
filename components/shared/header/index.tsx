47696cb8766f18b199715e606f57cca1ebd68f5c
idrissiradi <idrissiradi@gmail.com>
2025-02-22 18:56:29 +0100

    Update dependencies and enhance header with cart and sign-in buttons

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
+import { Button } from "@/components/ui/button";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import ModeToggle from "@/components/shared/header/mode-toggle";
 
 const Header = () => {
   return (
@@ -19,6 +22,20 @@ const Header = () => {
               {APP_NAME}
             </span>
           </Link>
+        </div>
+
+        <div className="space-x-2">
+          <ModeToggle />
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href="/sign-in">
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
         </div>
       </div>
     </header>
