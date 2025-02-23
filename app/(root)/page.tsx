c08ffdd43ba5631fb4743c7c5c268ceb3c0b102b
idrissiradi <idrissiradi@gmail.com>
2025-02-23 20:35:45 +0000

    Add Prisma integration for product management and update homepage to display latest products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
-import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const Homepage = () => {
+const Homepage = async () => {
+    const latestProducts = await getLatestProducts();
+
     return (
         <>
             <ProductList
-                data={sampleData.products}
+                data={latestProducts}
                 title="Newest Arrivals"
                 limit={4}
             />
