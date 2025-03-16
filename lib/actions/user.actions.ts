848c8aa54a81c462ae8c88b5ed5eab4a658a2747
idrissiradi <idrissiradi@gmail.com>
2025-03-16 09:02:39 +0000

    Add user sign-up page and form with validation; enhance session and JWT handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "@/lib/utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -42,16 +43,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
             confirmPassword: formData.get("confirmPassword"),
         });
 
-        const userExists = await prisma.user.findUnique({
-            where: {
-                email: user.email,
-            },
-        });
-
-        if (userExists) {
-            return { success: false, message: "User already exists!" };
-        }
-
         const plainPassword = user.password;
         user.password = hashSync(user.password, 10);
 
@@ -71,6 +62,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
         if (isRedirectError(error)) {
             throw error;
         }
-        return { success: false, message: "User was not registered" };
+        return { success: false, message: formatError(error) };
     }
 }
