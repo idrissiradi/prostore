aecea5d59371afcb8d044e6725b57ea2b5c6b816
idrissiradi <idrissiradi@gmail.com>
2025-04-11 20:10:03 +0100

    Add remove item functionality to cart with loading state and error handling

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,47 +1,93 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { CartItem } from "@/types";
+import { Cart, CartItem } from "@/types";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { ToastAction } from "@/components/ui/toast";
-import { Plus } from "lucide-react";
+import { Loader, Minus, Plus } from "lucide-react";
+import { useTransition } from "react";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
     const router = useRouter();
     const { toast } = useToast();
 
+    const [isPending, startTransition] = useTransition();
+
     const handleAddToCart = async () => {
-        const res = await addItemToCart(item);
+        startTransition(async () => {
+            const res = await addItemToCart(item);
 
-        if (!res.success) {
+            if (!res.success) {
+                toast({
+                    variant: "destructive",
+                    description: res.message,
+                    title: "Error",
+                });
+                return;
+            }
+
+            // Handle success add to cart
             toast({
-                variant: "destructive",
                 description: res.message,
-                title: "Error",
+                action: (
+                    <ToastAction
+                        className="bg-primary text-white hover:bg-gray-800"
+                        altText="Go to cart"
+                        onClick={() => router.push("/cart")}
+                    >
+                        Go to cart
+                    </ToastAction>
+                ),
             });
-            return;
-        }
-
-        // Handle success add to cart
-        toast({
-            description: res.message,
-            action: (
-                <ToastAction
-                    className="bg-primary text-white hover:bg-gray-800"
-                    altText="Go to cart"
-                    onClick={() => router.push("/cart")}
-                >
-                    Go to cart
-                </ToastAction>
-            ),
         });
     };
 
-    return (
+    // Handle remove from cart
+    const handleRemoveFromCart = async () => {
+        startTransition(async () => {
+            const res = await removeItemFromCart(item.productId);
+
+            toast({
+                variant: res.success ? "default" : "destructive",
+                description: res.message,
+            });
+
+            return;
+        });
+    };
+
+    // Check if item is in cart
+    const existItem =
+        cart && cart.items.find((i) => i.productId === item.productId);
+
+    return existItem ? (
+        <div>
+            <Button
+                type="button"
+                variant="outline"
+                onClick={handleRemoveFromCart}
+            >
+                {isPending ? (
+                    <Loader className="h-4 w-4 animate-spin" />
+                ) : (
+                    <Minus className="h-4 w-4" />
+                )}
+            </Button>
+            <span className="px-2">{existItem.qty}</span>
+            <Button type="button" variant="outline" onClick={handleAddToCart}>
+                {isPending ? (
+                    <Loader className="h-4 w-4 animate-spin" />
+                ) : (
+                    <Plus className="h-4 w-4" />
+                )}
+            </Button>
+        </div>
+    ) : (
         <Button className="w-full" type="button" onClick={handleAddToCart}>
-            <Plus /> Add To Cart
+            {isPending ? <Loader className="h-4 w-4 animate-spin" /> : <Plus />}{" "}
+            Add To Cart
         </Button>
     );
 };
